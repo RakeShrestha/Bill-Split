5fb6278961b81926dd1861800e8d1ac8121ec6d8
RakeShrestha <orakeshstha7@gmail.com>
2024-12-18 08:14:26 +0545

    Initial Commit

---
+++ SwiftUI-Form-NavigationStack/SwiftUI_Form_NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Form_NavigationStackApp.swift
+//  SwiftUI-Form-NavigationStack
+//
+//  Created by Rakesh Shrestha on 18/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Form_NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
