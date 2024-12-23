7710e00fd15d180ea8b68f8b3f14161039076111
RakeShrestha <orakeshstha7@gmail.com>
2024-12-23 08:09:50 +0545

    Learned how to build forms and sections, creating navigation stacks and navigation bar titles, how to store program state with the @State and @FocusState property wrappers, how to create user interface controls like TextField and Picker, and how to create views in a loop using ForEach

--- SwiftUI-Form-NavigationStack/ContentView.swift
+++ SwiftUI-Form-NavigationStack/ContentView.swift
@@ -8,34 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
-    let students = ["Ron", "Harry", "Hermione"]
-    @State private var selectedStudent = "Harry"
-    @State private var tapCount = 0
-    @State private var name = ""
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalAmount: Double {
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount * tipSelection / 100
+        let grandTotal = checkAmount + tipValue
+        return grandTotal
+    }
+    
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let amountPerPerson = totalAmount / peopleCount
+        return amountPerPerson
+    }
     
     var body: some View {
         NavigationStack {
             Form {
-                Picker("Select your student", selection: $selectedStudent) {
-                    ForEach(students, id: \.self) {
-                        Text($0)
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100){
+                            Text("\($0) People")
+                        }
                     }
                 }
-            }
-            .navigationTitle("Learning")
-            .navigationBarTitleDisplayMode(.inline)
-        }
-        VStack {
-            Button("Tap Count: \(tapCount)") {
-                self.tapCount += 1
-            }
-            Form {
-                TextField("Enter your name", text: $name)
-                ForEach(0..<5) {
-                    Text("Using dollar \($0)")
+                
+                Section("How much tip do you want to leave?") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        //                        ForEach(0..<101){
+                        //                            Text("\($0)%")
+                        //                        }
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
                 }
-                if name != "" {
-                    Text("Your name is \(name)")
+                
+                .pickerStyle(.navigationLink)
+                
+                Section("Total Amount") {
+                    Text(totalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+                Section("Amount per person") {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            
+            
+            .navigationTitle("Bill Split")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
                 }
             }
         }
